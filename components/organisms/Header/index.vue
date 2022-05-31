d0b183be76f3402988db9725e44d171be4dadedd
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-05-31 02:50:38 -0300

    feat: create header component

---
+++ components/organisms/Header/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <header>
+    <Container class="flex justify-between items-center py-2">
+      <div class="flex gap-1">
+        <button
+          class="text-primary-dark flex items-center gap bg-white cursor-pointer text-base"
+        >
+          <Icon name="info" color="#007a8d" />
+          Como funciona
+        </button>
+        <Divider vertical />
+        <Contact
+          class="text-primary-dark"
+          title="0800 123 2222"
+          subtitle="Envie mensagem ou ligue"
+        >
+          <img
+            width="32"
+            src="~/assets/icons/whatsapp.svg"
+            alt="ícone de whatsapp"
+          />
+        </Contact>
+      </div>
+      <Logo />
+      <Avatar
+        class="text-primary-dark"
+        username="Gabriel Caiana"
+        icon-color="#007a8d"
+      />
+    </Container>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader',
+}
+</script>
