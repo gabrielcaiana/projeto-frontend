c1016494d7e1c99442da5018d4d3a59734aac261
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-05-31 02:52:48 -0300

    chore: change name layout folder

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Header />
+    <main>
+      <Container>
+        <Nuxt />
+      </Container>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppLayoutDefault',
+}
+</script>
