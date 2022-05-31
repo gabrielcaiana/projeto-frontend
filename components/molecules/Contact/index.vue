b5d7aef7971bdb50ce37d9c52cea285a5206a396
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-05-31 02:50:53 -0300

    feat: create contact component

---
+++ components/molecules/Contact/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex items-center gap cursor-pointer">
+    <slot />
+    <div class="flex flex-col text-medium">
+      <p v-text="title"></p>
+      <span v-text="subtitle"></span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppContact',
+
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    subtitle: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
