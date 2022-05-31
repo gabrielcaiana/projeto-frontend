1ce98f3687f3a83a588c3284c88302bbdd513b6b
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-05-31 01:45:58 -0300

    feat: create avatar component

---
+++ components/molecules/avatar/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex gap items-center cursor-pointer">
+    <span
+      class="text-primary-dark text-medium text-base"
+      v-text="username"
+    ></span>
+    <Icon name="user" color="#007A8D" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppAvatar',
+
+  props: {
+    username: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
