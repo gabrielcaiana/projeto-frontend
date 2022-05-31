5587c8922987c6cec25312771cdb849ea7f91ebc
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-05-31 02:53:10 -0300

    chore: update avatar

--- components/molecules/avatar/index.vue
+++ components/molecules/avatar/index.vue
@@ -1,10 +1,7 @@
 <template>
   <div class="flex gap items-center cursor-pointer">
-    <span
-      class="text-primary-dark text-medium text-base"
-      v-text="username"
-    ></span>
-    <Icon name="user" color="#007A8D" />
+    <span class="text-medium text-sm" v-text="username"></span>
+    <Icon name="user" size="20" :color="iconColor" />
   </div>
 </template>
 
@@ -16,6 +13,11 @@ export default {
     username: {
       type: String,
       required: true,
+    },
+
+    iconColor: {
+      type: String,
+      default: '#007A8D',
     },
   },
 }
