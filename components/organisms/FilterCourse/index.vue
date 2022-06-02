477c14d898cc2ee5aec6b83e0c24ff506f60ed76
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-06-02 00:15:44 -0300

    refactor: wip

---
+++ components/organisms/FilterCourse/index.vue
@@ -0,0 +1,72 @@
+<template>
+  <Modal :show="show" title="Adicionar bolsa" @close="$emit('close')">
+    <Subtitle class="mt-1" text="Filtre e adicione as bolsas de seu intersse" />
+
+    <div class="pt-2 pb-5">
+      <div
+        :class="[
+          'mb-2 gap-1',
+          isDesktop ? 'flex items-center' : 'flex flex-col',
+        ]"
+      >
+        <InputSelect
+          label="Selecione sua cidade"
+          :options="citys"
+          @input="citySelected = $event"
+        />
+        <InputSelect
+          label="Selecione o curso de sua preferência"
+          :options="citys"
+          @input="courseSelected = $event"
+        />
+      </div>
+
+      <div :class="[isDesktop ? 'flex' : 'flex flex-col']">
+        <div class="w-full">
+          <p class="text-medium mb-2">COMO VOCÊ QUER ESTUDAR?</p>
+          <div :class="['flex gap-2', !isDesktop && 'mb-2']">
+            <Checkbox label="Presencial" @input="presencial = !presencial" />
+            <Checkbox label="A distância" @input="distancia = !distancia" />
+          </div>
+        </div>
+
+        <div :class="['w-full', isDesktop && 'ml-1']">
+          <p class="text-medium">ATÉ QUANTO PODE PAGAR?</p>
+          <span v-text="`R$${range}`"></span>
+          <Slider
+            v-model="range"
+            :align-unity-left="false"
+            label="teste"
+            min="0"
+            max="10000"
+            step="0.1"
+          />
+        </div>
+      </div>
+    </div>
+  </Modal>
+</template>
+
+<script>
+export default {
+  name: 'AppFilterCourse',
+
+  props: {
+    show: {
+      type: Boolean,
+      defaul: false,
+    },
+  },
+  data() {
+    return {
+      citys: ['São Paulo', 'São José dos campos', 'Rio de janeiro'],
+      courses: ['Direito', 'Medicina', 'Pedagogia'],
+      citySelected: '',
+      courseSelected: '',
+      presencial: false,
+      distancia: false,
+      range: 0,
+    }
+  },
+}
+</script>
--- components/organisms/ListCourses/index.vue
+++ components/organisms/ListCourses/index.vue
@@ -3,32 +3,10 @@
     <div class="w-full flex flex-end">
       <Tabs active="id2" />
     </div>
-    <div class="mt-3 flex items-center justify-center flex-wrap gap-1">
-      <Card empty @click="showModal = !showModal" />
-      <Card v-for="(i, index) in 4" :key="index" />
+    <div class="mt-3 flex items-center flex-wrap gap-1">
+      <Card empty @click="showFilterCourse = !showFilterCourse" />
     </div>
-
-    <Modal :show="showModal" title="Adicionar bolsa" @close="showModal = false">
-      <Subtitle text="Filtre e adicione as bolsas de seu intersse" />
-
-      <div class="pt-2 pb-5">
-        <div class="flex items-center gap-1">
-          <InputSelect
-            label="Selecione sua cidade"
-            :options="citys"
-            @input="citySelected = $event"
-          />
-          <InputSelect
-            label="Selecione o curso de sua preferência"
-            :options="citys"
-            @input="courseSelected = $event"
-          />
-        </div>
-
-        <br />
-        {{ citySelected }} - {{ courseSelected }}
-      </div>
-    </Modal>
+    <FilterCourse :show="showFilterCourse" @close="showFilterCourse = false" />
   </div>
 </template>
 
@@ -37,11 +15,7 @@ export default {
   name: 'AppListCourses',
   data() {
     return {
-      showModal: false,
-      citys: ['São Paulo', 'São José dos campos', 'Rio de janeiro'],
-      courses: ['Direito', 'Medicina', 'Pedagogia'],
-      citySelected: '',
-      courseSelected: '',
+      showFilterCourse: false,
     }
   },
 }
