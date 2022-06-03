2844a4c74e5670d52a06e57de6031bf0af3a23f9
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-06-02 23:34:41 -0300

    chore: test

--- plugins/dataApi.js
+++ plugins/dataApi.js
@@ -2,7 +2,9 @@ export default function ({ $axios }, inject) {
   const get = async () => {
     let response
     try {
-      response = await $axios.$get('/db.json')
+      response = await $axios.$get('/db.json', {
+        headers: { 'Access-Control-Allow-Origin': '*' },
+      })
     } catch (error) {
       console.log(error)
     }
